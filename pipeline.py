039d74d755de4f2468cdcdcbf52ed1702ff5a2a6
lucasmunizsg <lucasmunizrj78@gmail.com>
2023-10-13 11:57:20 -0300

    Mudança de IA generativa

--- pipeline.py
+++ pipeline.py
@@ -13,13 +13,12 @@ sdw2023_api_url = 'https://sdw-2023-prd.up.railway.app'
 import pandas as pd
 import requests
 import json
-import openai
+# import openai
 
 
 df = pd.read_csv('SDW2023.csv')
 user_ids = df['UserID'].tolist()
 print(user_ids)
-
 
 
 def get_user(id):
@@ -29,35 +28,6 @@ def get_user(id):
 users = [user for id in user_ids if (user := get_user(id)) is not None]
 print(json.dumps(users, indent=2))
 
-# AGUARDANDO VALIDAÇÃO DA CHAVE APOS O PAGAMENTO 1K DE TOKENS IN E OUTPUT
-openai_api_key = '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'
-
-openai.api_key = openai_api_key
-
-def generate_ai_news(user):
-  completion = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[
-      {
-          "role": "system",
-          "content": "Você é um especialista em markting bancário."
-      },
-      {
-          "role": "user",
-          "content": f"Crie uma mensagem para {user['name']} sobre a importância dos investimentos (máximo de 100 caracteres)"
-      }
-    ]
-  )
-  return completion.choices[0].message.content.strip('\"')
-
-for user in users:
-  news = generate_ai_news(user)
-  print(news)
-  user['news'].append({
-      "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
-      "description": news
-  })
-
 def update_user(user):
     response = requests.put(f"{sdw2023_api_url}/users/{user['id']}", json=user)
     return True if response.status_code == 200 else False
@@ -65,3 +35,47 @@ def update_user(user):
 for user in users:
   success = update_user(user)
   print(f"User {user['name']} updated? {success}!")
+
+# OPÇÃO COM API DA DEEP IA
+import requests
+r = requests.post(
+    "https://api.deepai.org/api/text-generator",
+    data={
+        'text': "Crie uma mensagem para {user['name']} sobre a importância dos investimentos (máximo de 100 caracteres)",
+    },
+    headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
+)
+print(r.json())
+# =====================================================================
+
+
+
+
+# OPÇÃO COM A API DA OPENAI
+# openai_api_key = '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'
+
+# openai.api_key = openai_api_key
+
+# def generate_ai_news(user):
+#   completion = openai.ChatCompletion.create(
+#     model="gpt-4",
+#     messages=[
+#       {
+#           "role": "system",
+#           "content": "Você é um especialista em markting bancário."
+#       },
+#       {
+#           "role": "user",
+#           "content": f"Crie uma mensagem para {user['name']} sobre a importância dos investimentos (máximo de 100 caracteres)"
+#       }
+#     ]
+#   )
+#   return completion.choices[0].message.content.strip('\"')
+
+# for user in users:
+#   news = generate_ai_news(user)
+#   print(news)
+#   user['news'].append({
+#       "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
+#       "description": news
+#   })
